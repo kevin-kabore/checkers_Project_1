4954ac351544895ae70a4ada6d9513cbbfe3fce8
Kevin Kabore <kevin.s.kabore@gmail.com>
2016-07-05 12:47:12 -0700

    Adds colors to board

--- js/main.js
+++ js/main.js
@@ -1,16 +1,37 @@
 // console.log("Javascript connected");
 var board = []
-
-var $table = $('table')
-for (var i = 0; i < 8; i++) {
-  var $newTr = $('<tr>')
-  for (var j = 0; j < 8; j++) {
-    var $newTD = $('<td>')
-    $newTr.append($newTD);
+function createTable() {
+  var $table = $('table')
+  for (var i = 0; i < 8; i++) {
+    var $newTr = $('<tr>')
+    board.push([])
+    for (var j = 0; j < 8; j++) {
+      var $newTD = $('<td>')
+      $newTD.addClass("tile")
+      $newTr.append($newTD);
+      board[i].push($newTD)
+    }
+    $table.append($newTr)
   }
-  $table.append($newTr)
-
-  // for loop
 }
+createTable();
 
-board.push($table);
+function colorBoard(){
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board.length; j++) {
+      if ((i+j)%2) {
+        board[i][j].addClass('black');
+        // if i between 0 & 2 add piece
+      } else {
+        board[i][j].addClass('red');
+        //if 5 & 7 add a piece
+      }
+    }
+  }
+}
+colorBoard()
+
+function addPieces(){
+
+}
+addPieces();
