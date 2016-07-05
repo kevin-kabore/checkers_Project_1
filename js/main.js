75c9ae7c1161e8b2b2ac7da1b79a91259dc85cab
Kevin Kabore <kevin.s.kabore@gmail.com>
2016-07-05 16:40:14 -0700

    Creating move function

--- js/main.js
+++ js/main.js
@@ -1,47 +1,50 @@
 // console.log("Javascript connected");
-// <img src="' + src + '" />'
 
 var $blackPiece = "url('assets/black_piece.png')"
+var $whitePiece = "url('assets/white_piece.png')"
+var $board = []
 
-var board = []
 function createTable() {
   var $table = $('table')
   for (var i = 0; i < 8; i++) {
     var $newTr = $('<tr>')
-    board.push([])
+    $board.push([])
     for (var j = 0; j < 8; j++) {
       var $newTD = $('<td>')
       $newTD.addClass("tile")
       $newTr.append($newTD);
-      board[i].push($newTD)
+      $board[i].push($newTD)
     }
     $table.append($newTr)
   }
 }
 createTable();
 
-function colorBoard(){
-  for (var i = 0; i < board.length; i++) {
-    for (var j = 0; j < board.length; j++) {
+function setBoard(){
+  for (var i = 0; i < $board.length; i++) {
+    for (var j = 0; j < $board.length; j++) {
       if ((i+j)%2) {
-        board[i][j].addClass('black');
-
-        if (i < 2 ) {
-
-        }
+        $board[i][j].addClass('black');
       } else {
-        board[i][j].addClass('red');
-        //if 5 & 7 add a piece
+        $board[i][j].addClass('red');
         if ( i >= 5 && i <=  7) {
-          board[i][j].css('background-image', $blackPiece);
+          $board[i][j].css('background-image', $blackPiece);
+        } else if (i <= 2 ) {
+          $board[i][j].css('background-image', $whitePiece);
         }
       }
     }
   }
 }
-colorBoard()
+setBoard()
 
-function addPieces(){
-
-}
-addPieces();
+function makeMove(){
+  for (var i = 0; i < $board.length; i++) {
+    for (var j = 0; j < $board.length; j++) {
+      $board[i][j].click(function(){
+        alert('You clicked')
+      })
+    }
+  }
+ }
+makeMove();
