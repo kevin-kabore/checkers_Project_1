2d8bcbb7b045bc4164a01bf202dd38b0c5bdcbf9
Kevin Kabore <kevin.s.kabore@gmail.com>
2016-07-05 23:51:54 -0700

    moves pieces

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 // console.log("Javascript connected");
 
-var $blackPiece = "url('assets/black_piece.png')"
-var $whitePiece = "url('assets/white_piece.png')"
+
 var $board = []
 
 function createTable() {
@@ -19,18 +18,24 @@ function createTable() {
   }
 }
 createTable();
-
+var counter = 0;
 function setBoard(){
+
   for (var i = 0; i < $board.length; i++) {
     for (var j = 0; j < $board.length; j++) {
       if ((i+j)%2) {
         $board[i][j].addClass('black');
+        $board[i][j].click(selectContent)
+
       } else {
         $board[i][j].addClass('red');
+        $board[i][j].click(selectContent)
         if ( i >= 5 && i <=  7) {
-          $board[i][j].css('background-image', $blackPiece);
+          $board[i][j].addClass('blackPieces');
+
         } else if (i <= 2 ) {
-          $board[i][j].css('background-image', $whitePiece);
+          $board[i][j].addClass('whitePieces');
+
         }
       }
     }
@@ -38,13 +43,44 @@ function setBoard(){
 }
 setBoard()
 
-function makeMove(){
-  for (var i = 0; i < $board.length; i++) {
-    for (var j = 0; j < $board.length; j++) {
-      $board[i][j].click(function(){
-        alert('You clicked')
-      })
+
+
+
+//
+// $('.myClass').click(function() {
+//   var clicks = $(this).data('clicks');
+//   if (clicks) {
+//      // odd clicks
+//   } else {
+//      // even clicks
+//   }
+//   $(this).data("clicks", !clicks);
+// });
+
+var secondClick;
+var content;
+var whiteCounter = 1;
+var blackCounter = 1;
+
+//Still need a way to keep one single variable to keep track of turns
+function selectContent(){
+
+  if ($(this).hasClass("whitePieces") && (whiteCounter%2 !== 0)) {
+    //add logic to check  for bad moves
+      $(this).removeClass("whitePieces");
+      whiteCounter= whiteCounter + 1;
+    } else if(!$(this).hasClass('whitePieces') && whiteCounter%2 == 0) {
+      $(this).addClass("whitePieces")
+      whiteCounter = whiteCounter +1;
+    }
+
+
+  if ($(this).hasClass("blackPieces") && (blackCounter%2 !== 0)) {
+    //Add logic to check for bad moves
+      $(this).removeClass("blackPieces");
+      blackCounter= blackCounter + 1;
+    } else if(!$(this).hasClass('blackPieces') && blackCounter%2 == 0) {
+      $(this).addClass("blackPieces")
+      blackCounter = blackCounter +1;
     }
   }
- }
-makeMove();
